3d6715eaa4b689982dd5a7783d4cd95ef326b327
seiya <r108338@yahoo.co.jp>
2020-11-24 00:23:41 +0900

    add dampening for SGD (lr=1e-6)

--- src/optimizers/optimizers.py
+++ src/optimizers/optimizers.py
@@ -6,7 +6,7 @@ from ..utils import kw
 
 fig3_optims = {
     "SGD + Momentum": kw(optim.SGD, lr=10**-3, momentum=0.9, dampening=0.9),
-    "SGD + Momentum (α=10^-6)": kw(optim.SGD, lr=10**-6, momentum=0.9),
+    "SGD + Momentum (α=10^-6)": kw(optim.SGD, lr=10**-6, momentum=0.9, dampening=0.9),
     "Adam": optim.Adam,
     "AdaBelief": kw(AdaBelief, rectify=False),
 }
